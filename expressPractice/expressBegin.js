0d6326f007b064534a37060a480c77f95c5bc5cb
matdol <wnsdn0308@gmail.com>
2019-08-30 22:27:35 +0900

    add

---
+++ expressPractice/expressBegin.js
@@ -0,0 +1,9 @@
+// 1. npm i -g express-generator
+// --> 기본 폴더 구조를 잡아줌. 설치하기가 편하게끔.
+
+// 2. express [프로젝트 명] --view=pug 
+
+// 3. cd [프로젝트 명]
+// 4. npm -i  --> package.json 에 적혀있는 dependency 들이 한 번에 설치됨.
+
+// 5. npm start  --> 기본 포트가 3000 으로 설정되어 있움.
